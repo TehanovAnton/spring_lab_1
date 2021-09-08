0dd28907a4709149a534c56f7d80d2b7d4908d51
antony <tehanovanton@gmail.com>
2021-09-08 17:51:25 +0300

    add_book page work

--- src/main/java/com/example/lab1/controller/BookController.java
+++ src/main/java/com/example/lab1/controller/BookController.java
@@ -1,9 +1,11 @@
 package com.example.lab1.controller;
 
+import com.example.lab1.forms.BookForm;
 import com.example.lab1.model.Book;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -32,11 +34,39 @@ public class BookController {
         return modelAndView;
     }
 
-    @RequestMapping(value = { "/personalList" }, method = RequestMethod.GET)
+    @RequestMapping(value = { "/personal_list" }, method = RequestMethod.GET)
     public ModelAndView personalList(Model model) {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("book_list");
         model.addAttribute("books", books);
+        return modelAndView;
+    }
+
+    @RequestMapping(value = { "/add_book_page" }, method = RequestMethod.GET)
+    public ModelAndView showAddBookPage(Model model) {
+        ModelAndView modelAndView = new ModelAndView("add_book");
+        BookForm bookForm = new BookForm();
+        model.addAttribute("bookform", bookForm);
+        return modelAndView;
+    }
+
+    @RequestMapping(value = { "/add_book_page" }, method = RequestMethod.POST)
+    public ModelAndView savePerson(Model model, @ModelAttribute("bookform") BookForm bookForm) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("book_list");
+
+        String title = bookForm.getTitle();
+        String author = bookForm.getAuthor();
+
+        if (title != null && title.length() > 0 && author != null && author.length() > 0) {
+            Book newBook = new Book(title, author);
+            books.add(newBook);
+            model.addAttribute("books",books);
+            return modelAndView;
+        }
+
+        model.addAttribute("errorMessage", errorMessage);
+        modelAndView.setViewName("add_book");
         return modelAndView;
     }
 }
